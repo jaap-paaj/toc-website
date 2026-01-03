9d5ea842bca1447c08fec2343b06ac158d008188
Jaap Beärda <jaapbearda@MacBook-Air-van-Jaap.local>
2026-01-03 16:21:54 +0100

    Fix: use GA4 measurement ID env var

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
                   anonymize_ip: true
                 });
               `}
